1cc4aa467cdb0e6141da6036ac982388e12aa35e
DIFINN <122594198+Difinn@users.noreply.github.com>
2025-04-02 16:00:55 +0300

    GBrfgewrSfdsfsd

--- main/tabl_creation.py
+++ main/tabl_creation.py
@@ -1,6 +1,14 @@
 import sqlite3
-connection = sqlite3.connect('userstable.db')
-cursor = connection.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARU KEY, name TEXT NOT NULL, birth date TEXT NOT NULL, role TEXT NOT NULL)')
-connection.commit()
-connection.close()
+db = sqlite3.connect("userstable.db")
+
+c = db.cursor()
+
+c.execute("""CREATE TABLE articles (
+    id text,
+    name text,
+    birth_date text,
+    role text
+)""")
+
+db.commit()
+db.close()
